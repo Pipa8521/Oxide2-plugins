dea2e4e0358248f65462cbf19a72be3ad9d550b7
Prefix <prefixltu@gmail.com>
2016-03-17 17:00:30 +0200

    TextFX -> Reindent

--- Legacy/TeleportGlitchFix.cs
+++ Legacy/TeleportGlitchFix.cs
@@ -10,39 +10,39 @@ using Oxide.Core.Plugins;
 
 namespace Oxide.Plugins
 {
-    [Info("TeleportGlitchFix", "Prefix", "1.0.0")]
-    class TeleportGlitchFix : RustLegacyPlugin
-    {
-        [PluginReference]
-        Plugin Share;
+	[Info("TeleportGlitchFix", "Prefix", "1.0.0")]
+	class TeleportGlitchFix : RustLegacyPlugin
+	{
+		[PluginReference]
+		Plugin Share;
 		
 		static float distance = 50.0f;
 		
-        /*[ChatCommand("checkwhere")]
-        void cmdCheckWhere(NetUser netuser, string command, string[] args)
-        {
+		/*[ChatCommand("checkwhere")]
+		void cmdCheckWhere(NetUser netuser, string command, string[] args)
+		{
 			object obj = ClosestBuilding(netuser);
 			if(obj is float) {
 				SendReply(netuser, string.Format( "Closest building is {0}", (float)obj));
 			} else {
 				SendReply(netuser, "There aren't any buildings in game.");
 			}
-        }*/
+		}*/
 		
 		void LoadDefaultConfig() { }
 
-        private void CheckCfg<T>(string Key, ref T var)
-        {
-            if (Config[Key] is T)
-                var = (T)Config[Key];
-            else
-                Config[Key] = var;
-        } 
+		private void CheckCfg<T>(string Key, ref T var)
+		{
+			if (Config[Key] is T)
+			var = (T)Config[Key];
+			else
+			Config[Key] = var;
+		} 
 		
-        void Init()
-        {
-            CheckCfg<float>("Distance between player and building", ref distance);
-            SaveConfig();
+		void Init()
+		{
+			CheckCfg<float>("Distance between player and building", ref distance);
+			SaveConfig();
 		}
 		
 		object canTeleport(NetUser netuser) {
@@ -61,7 +61,7 @@ namespace Oxide.Plugins
 		
 		object ClosestBuilding(NetUser netuser) {
 			
-            PlayerClient playerclient = netuser.playerClient;
+			PlayerClient playerclient = netuser.playerClient;
 			Vector3 lastPosition = playerclient.lastKnownPosition;
 			Vector2 v1 = new Vector2(lastPosition.x, lastPosition.z);
 			float dist = 0.0f;
@@ -74,15 +74,15 @@ namespace Oxide.Plugins
 				{
 					// If building owner is netuser > skip
 					if(userid.Equals(master.ownerID.ToString()))
-						continue;
+					continue;
 					// If using share plugin and it's sharing
-                    if(Share != null)
-                    {
-                        if((bool)Share.Call("isSharing", userid, master.ownerID.ToString()))
-                        {
+					if(Share != null)
+					{
+						if((bool)Share.Call("isSharing", userid, master.ownerID.ToString()))
+						{
 							continue;
-                        }
-                    }
+						}
+					}
 					
 					Vector2 v2 = new Vector2(master.transform.position.x, master.transform.position.z);
 					if (!set) {
