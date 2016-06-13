879501de88f5c062ffda8cb22bf3d8e3ccd3f116
PreFiX <prefixltu@gmail.com>
2016-06-13 11:00:23 +0300

    TEXT FX REINTEND

--- Legacy/Report.cs
+++ Legacy/Report.cs
@@ -3,18 +3,18 @@ using Oxide.Core;
 
 namespace Oxide.Plugins
 {
-    [Info("Report", "Prefix", "1.0.0")]
-    class Report : RustLegacyPlugin
-    {
+	[Info("Report", "Prefix", "1.0.0")]
+	class Report : RustLegacyPlugin
+	{
 		const string ChatTag = "Report";
 		const string MyPerm = "report.cansee";
 		
 		void Init() {
 			if (!permission.PermissionExists(MyPerm)) permission.RegisterPermission(MyPerm, this);
 		}
-        [ChatCommand("report")]
-        void Command_Report(NetUser netuser, string command, string[] args)
-        {
+		[ChatCommand("report")]
+		void Command_Report(NetUser netuser, string command, string[] args)
+		{
 			if(args.Length < 2) { rust.SendChatMessage(netuser, ChatTag, "[color red]Use /report nick reason - to report someone."); return; }
 			
 			NetUser target = rust.FindPlayer(args[0]);
@@ -26,11 +26,11 @@ namespace Oxide.Plugins
 				for (int i = 0; i < netusers.Count; i++)
 				{
 					if(permission.UserHasPermission(netusers[i].userID.ToString(), MyPerm))
-						rust.SendChatMessage(netusers[i], ChatTag, "Player {0} reported {1} for {2}", rust.QuoteSafe(netuser.displayName), rust.QuoteSafe(target.displayName), reason);
+					rust.SendChatMessage(netusers[i], ChatTag, "Player {0} reported {1} for {2}", rust.QuoteSafe(netuser.displayName), rust.QuoteSafe(target.displayName), reason);
 				}
 			}
 			
-        } 
+		} 
 		static string StringArrayToString(string[] array)
 		{
 			string result = string.Join(" ", array.Where(item => !string.IsNullOrEmpty(item)).ToArray());
